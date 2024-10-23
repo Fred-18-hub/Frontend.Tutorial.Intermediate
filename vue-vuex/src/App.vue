bc6168f089e4336d81817957da9fb1d83ec76a4a
Fredrick Appiah <fredrick.antwi@hubtel.com>
2024-10-23 12:56:17 +0000

    vuex setup

---
+++ vue-vuex/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>State Management with Vuex</h2>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  background: #eee;
+}
+</style>
